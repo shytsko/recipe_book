ffab90aa8df1d4dc7d01f990bc2cebbfe22006cb
Vladimir Shytsko <shytsko.v@gmail.com>
2023-10-17 14:10:36 +0300

    Add 404 page, add menu items

--- src/src/settings.py
+++ src/src/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-6!*77g+*rcnra%a0vic$%tvunupktwwqr+f65kaict-x83thek
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 # Application definition
 
