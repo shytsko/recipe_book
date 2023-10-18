040a098086ca86b2874ea41832749593c9a1f70b
Vladimir Shytsko <shytsko.v@gmail.com>
2023-10-18 14:55:43 +0300

    Add view all recipe

--- src/recipes/urls.py
+++ src/recipes/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import RecipeCreateView, RecipeDetailView
+from .views import RecipeCreateView, RecipeDetailView, RecipeListAllViewBase
 
 urlpatterns = [
     path('create/', RecipeCreateView.as_view(), name='recipe_create'),
     path('<int:recipe_id>/', RecipeDetailView.as_view(), name='recipe_detail'),
+    path('list-all/', RecipeListAllViewBase.as_view(), name='recipe_list_all'),
     # path('<int:workplace_id>/update', WorkplaceUpdateView.as_view(), name='workplace_update'),
     # path('<int:workplace_id>/delete', WorkplaceDeleteView.as_view(), name='workplace_delete'),
 ]
--- src/recipes/views.py
+++ src/recipes/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DetailView
+from django.views.generic.list import ListView
 from .forms import RecipeForm
 from .models import Recipe
 
@@ -13,9 +14,34 @@ class RecipeCreateView(LoginRequiredMixin, CreateView):
         form.instance.author = self.request.user
         return super(RecipeCreateView, self).form_valid(form)
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Новый рецепт'
+        return context
+
 
 class RecipeDetailView(DetailView):
     model = Recipe
     context_object_name = 'recipe'
     template_name = 'recipes/recipe_detail.html'
     pk_url_kwarg = 'recipe_id'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = self.object.name
+        return context
+
+
+class RecipeListViewBase(ListView):
+    model = Recipe
+    template_name = 'recipes/recipe_list.html'
+    title = ''
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = self.title
+        return context
+
+
+class RecipeListAllViewBase(RecipeListViewBase):
+    title = 'Все рецепты'
