8fda7e7d7f521d8cc6e9360d57c68231b7c3e148
Vladimir Shytsko <92379291+shytsko@users.noreply.github.com>
2023-11-01 08:41:44 +0300

    Update context_processors.py

--- src/recipes/context_processors.py
+++ src/recipes/context_processors.py
@@ -2,7 +2,6 @@ from .models import Category
 
 
 def categories(request):
-    """Добавляет в контекст переменную greeting с приветствием."""
     return {
         'categories': Category.objects.all()
     }
